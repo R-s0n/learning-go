3a0cb23f8b4f921cb210d58aec8d95e5334b481f
rs0n <rs0n@kali-box.kali-box.xya>
2024-09-02 12:42:41 -0500

    improve scanner

--- learning-go/tus-proto/scan/scan.go
+++ learning-go/tus-proto/scan/scan.go
@@ -3,18 +3,30 @@ package scan
 import (
 	"fmt"
 	"net/http"
+	"reflect"
 	"sync"
 )
 
 func Test(u string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Println("[-] Scanning " + u)
-	resp, err := http.Get("http://example.com/")
+	resp, err := http.Get(u)
 	if err != nil {
 		fmt.Println("[!] " + u + " did not respond to a GET request!")
-		fmt.Println(err)
+		// fmt.Println(err)
 		return
 	}
 	defer resp.Body.Close()
-	fmt.Println(resp.StatusCode)
+	msg := fmt.Sprintf("[+] "+u+" responded with status code %d", resp.StatusCode)
+	fmt.Println(msg)
+	fmt.Println(resp.Cookies())
+	fmt.Println(resp.Proto)
+	fmt.Println(reflect.TypeOf(resp.Header))
+	/*
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("[!] Could not read response body!")
+		}
+		fmt.Println(string(b))
+	*/
 }
