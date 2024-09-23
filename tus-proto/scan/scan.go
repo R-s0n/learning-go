2a7f84843266cdb8e5dd74897fdbe58f0032b370
rs0n <rs0n@kali-box-1.xyz>
2024-09-22 20:21:08 -0500

    add json names to custom struct

--- tus-proto/scan/scan.go
+++ tus-proto/scan/scan.go
@@ -27,17 +27,13 @@ func Test(u string, wg *sync.WaitGroup) {
 	msg := fmt.Sprintf("[+] "+u+" responded with status code %d", resp.StatusCode)
 	fmt.Println(msg)
 	c.Code = int(resp.StatusCode)
-	// fmt.Println(resp.Proto)
 	c.Protocol = resp.Proto
-	// fmt.Println(resp.Cookies())
 	c.Cookies = resp.Cookies()
-	// fmt.Println(resp.Header)
 	c.Headers = resp.Header
 	b, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("[!] Could not read response body!")
 	}
-	// fmt.Println(string(b))
 	c.Body = string(b)
 	fmt.Println(c)
 }
--- tus-proto/structs/structs.go
+++ tus-proto/structs/structs.go
@@ -5,9 +5,9 @@ import (
 )
 
 type TargetUrl struct {
-	Code     int
-	Protocol string
-	Cookies  []*http.Cookie
-	Headers  http.Header
-	Body     string
+	Code     int            `json:"code"`
+	Protocol string         `json:"protocol"`
+	Cookies  []*http.Cookie `json:"cookies"`
+	Headers  http.Header    `json:"headers"`
+	Body     string         `json:"body"`
 }
